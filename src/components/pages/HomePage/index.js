194cf26fa02edd09daacefa168a5f60ad971df18
cdssw <cdssw@naver.com>
2022-10-13 16:57:52 +0900

    누락 코드 추가

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
 import { PageTemplate, Header, CardList, Search } from "components";
 import { useHistory, Redirect } from "react-router-dom";
