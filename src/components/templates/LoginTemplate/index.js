daa355835b5d14c19137f9df320f2818bf1c7ce5
cdssw <cdssw@naver.com>
2022-10-14 11:27:24 +0900

    최소 높이 제거

--- src/components/templates/LoginTemplate/index.js
+++ src/components/templates/LoginTemplate/index.js
@@ -7,7 +7,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     height: '100vh',
-    minHeight: '600px',
   }, 
   headerWrap: {
     flexBasis: '50px',
